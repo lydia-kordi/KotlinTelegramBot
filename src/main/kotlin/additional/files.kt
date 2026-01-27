99c440f9a534a58e0f09855298e4babd7a439c13
korodiy <korodiy@yandex-team.ru>
2026-01-27 22:40:41 +0300

    KTB-02-create-text-file: Исправила замечания. try-catch гуглила, тк в курсе не было - надеюсь, правильно вышло

--- src/main/kotlin/additional/files.kt
+++ src/main/kotlin/additional/files.kt
@@ -1,15 +1,17 @@
 package additional
 
 import java.io.File
+import java.io.IOException
 
 fun main() {
+    val wordsFile = File("words.txt")
 
-    val wordsFile: File = File("words.txt")
-    wordsFile.createNewFile()
-
-    val lines = wordsFile.readLines()
-
-    for (line in lines) {
-        println(line)
+    try {
+        val lines = wordsFile.readLines()
+        for (line in lines) {
+            println(line)
+        }
+    } catch (e: IOException) {
+        println("Ошибка при чтении файла: ${e.message}")
     }
 }
