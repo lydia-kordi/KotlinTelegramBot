e8cb4bf57a48d67df76e050f2ec8833c80151ce1
korodiy <korodiy@yandex-team.ru>
2026-02-16 12:33:18 +0300

    KTB-13: Получение апдейтов от ТГ-бота, добавлена константа

--- src/main/kotlin/additional/Telegram.kt
+++ src/main/kotlin/additional/Telegram.kt
@@ -5,6 +5,8 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+const val TELEGRAM_BASE_URL = "https://api.telegram.org/bot"
+
 fun main(args: Array<String>) {
 
     val botToken = args[0]
@@ -24,7 +26,7 @@ fun main(args: Array<String>) {
 }
 
 fun getUpdates(botToken: String, updateID: Int): String {
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateID"
+    val urlGetUpdates = "$TELEGRAM_BASE_URL$botToken/getUpdates?offset=$updateID"
     val builder = HttpClient.newBuilder()
     val client = builder.build()
     val requestGeUpdates: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
