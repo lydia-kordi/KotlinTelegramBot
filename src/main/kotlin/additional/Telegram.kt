2094ac083e22320e9fc113fef0c16efe0fc82ff2
korodiy <korodiy@yandex-team.ru>
2026-02-16 12:30:09 +0300

    KTB-12: Токен авторизации Telegram + HTTP-запросы-Вторые правки

--- src/main/kotlin/additional/Telegram.kt
+++ src/main/kotlin/additional/Telegram.kt
@@ -5,24 +5,24 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+const val TELEGRAM_BASE_URL = "https://api.telegram.org/bot"
 
 fun main(args: Array<String>) {
 
     val botToken = args[0]
-    val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
+    val urlGetMe = "$TELEGRAM_BASE_URL$botToken/getMe"
+    val urlGetUpdates = "$TELEGRAM_BASE_URL$botToken/getUpdates"
 
     val builder = HttpClient.newBuilder()
     val client = builder.build()
 
     val requestGetMe: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetMe)).build()
-    val requestGeUpdates: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
+    val requestGetUpdates: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
 
     val responseGetMe: HttpResponse<String> = client.send(requestGetMe, HttpResponse.BodyHandlers.ofString())
-    val responseGetUpdates: HttpResponse<String> = client.send(requestGeUpdates, HttpResponse.BodyHandlers.ofString())
+    val responseGetUpdates: HttpResponse<String> = client.send(requestGetUpdates, HttpResponse.BodyHandlers.ofString())
 
     println(responseGetMe.body())
     println(responseGetUpdates.body())
-
 
 }
