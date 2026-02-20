e667ffcaa94ad3f7e6d1f00108b429458e9692d1
korodiy <korodiy@yandex-team.ru>
2026-02-20 23:12:06 +0300

    KTB-18: реализация изучения слов + рефакторинг

--- src/main/kotlin/additional/Main.kt
+++ src/main/kotlin/additional/Main.kt
@@ -17,8 +17,9 @@ fun main(args: Array<String>) {
 
     val updateIdRegex = "\"update_id\"\\s*:\\s*(\\d+)".toRegex()
     val chatIdRegex = "\"chat\"\\s*:\\s*\\{\\s*\"id\"\\s*:\\s*(\\d+)".toRegex()
+    val messageIdRegex = "\"message_id\"\\s*:\\s*(\\d+)".toRegex()
     val messageTextRegex = "\"text\"\\s*:\\s*\"(.+?)\"".toRegex()
-    val callbackRegex = "\"data\"\\s*:\\s*\"(.+?)\"".toRegex()   // ← ВОТ ЗДЕСЬ ИСПРАВЛЕНО
+    val callbackRegex = "\"data\"\\s*:\\s*\"(.+?)\"".toRegex()
 
     var updateId = 0
 
@@ -32,26 +33,33 @@ fun main(args: Array<String>) {
         }
 
         val chatId = chatIdRegex.find(updates)?.groupValues?.get(1)
+        val messageId = messageIdRegex.find(updates)?.groupValues?.get(1)?.toInt()
         val text = messageTextRegex.find(updates)?.groups?.get(1)?.value
         val callback = callbackRegex.find(updates)?.groups?.get(1)?.value
         val normalizedText = text?.trim()?.lowercase()?.replace(Regex("\\s+"), " ")
 
         if (chatId != null) {
 
-            if (callback != null) {
-                println("callback=$callback")
-                botService.handleCallback(chatId, callback)
-            }
+            if (callback != null && messageId != null) {
+                println("chatId = $chatId, messageId = $messageId, callback=$callback")
+                botService.handleCallback(chatId, messageId, callback)
 
-            else if (normalizedText != null) {
+            } else if (normalizedText != null) {
                 println("message=$text")
 
                 when {
                     normalizedText.startsWith(HELLO_COMMAND) ->
-                        botService.sendMenu(chatId)
+                        botService.handleStart(chatId)
 
                     else ->
-                        botService.sendMessage(chatId, "Такой команды не существует")
+                        botService.sendResponse(
+                            chatId,
+                            null,
+                            BotResponse(
+                                text = "Такой команды не существует :(",
+                                keyboard = listOf(listOf(TelegramButton("🔙 Назад", "back")))
+                            )
+                        )
                 }
             }
         }
